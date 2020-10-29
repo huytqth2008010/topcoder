0212540730276641462d7d4f7d0efe07a49d46ed
huytqth2008010 <truongquanghuybx0914@gmail.com>
2020-10-29 22:07:03 +0700

    huy cap nhat

---
+++ js/col-md-8.js
@@ -0,0 +1,106 @@
+function col_md_8() {
+    return "<div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s1.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Mẹo Vặt</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Mẹo vặt với rượu, các bà nội trợ đã biết chưa?</a></h3><br/>\n" +
+        "                                <p>Rượu là một loại đồ uống quen thuộc với cánh mày râu. Không chỉ có tác dụng tốt trong việc “giải sầu”, rượu còn c ó nhiều tác dụng bất ngờ...</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\">Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s2.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Review</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Đột kích quán Hotdog có anh chủ là người Hàn Quốc đẹp trai</a></h3><br/>\n" +
+        "                                <p>Tiêu chí để đánh giá chất lượng món ăn của bạn là gì? Đồ ăn ngon miệng, phục vụ thân thiện hay không gian sạch đẹp? Nếu tiêu chí của...</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\">Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s3.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Mẹo Vặt</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Khám phá công dụng kỳ diệu của nước vo gạo</a></h3><br/>\n" +
+        "                                <p>Nước vo gạo đã quá quen thuộc với mọi gia đình. Thông thường khi nấu cơm, nước vo gạo sẽ bị bỏ đi mà không ai biết được những tác dụng...</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\">Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s4.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Bí Quyết</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Lột xác với mật ong chỉ sau một đêm</a></h3><br/>\n" +
+        "                                <p>Làm đẹp bằng nguyên liệu tự nhiên là phương pháp được chị em ưu tiên hàng đầu. Trong đó, mật ong là một trong những nguyên liệu từ thiên nhiên...</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\" >Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s5.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Tự Nhiên</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Mở Spa tại nhà với những cách làm đẹp đơn giản từ Nha đam</a></h3><br/>\n" +
+        "                                <p>Nha đam được biết đến như một thần dược làm đẹp của chị em phụ nữ. Có quá nhiều lời đồn thổi về công dụng tuyệt vời của nha đam....</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\" >Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"main-left-1\">\n" +
+        "                        <div class=\"main-left-img\">\n" +
+        "                            <a href=\"#\">\n" +
+        "                                <img class=\"anh-s\" src=\"img/s6.jpg\" height=\"300\" width=\"300\"/>\n" +
+        "                            </a>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"main-left-text\">\n" +
+        "                            <div class=\"text-top\">\n" +
+        "                                <a href=\"#\" style=\"text-decoration: none\">Khám Phá</a>\n" +
+        "                            </div>\n" +
+        "                            <div class=\"text-main\">\n" +
+        "                                <h3 class=\"hover-line\"><a href=\"#\" class=\"text-main-a\" style=\"text-decoration: none\">Những món phải thử khi đi du lịch Huế</a></h3><br/>\n" +
+        "                                <p>Huế là một địa danh nổi tiếng không chỉ bởi nhiều danh lam thắng cảnh hay di tích lịch sử mà còn nổi tiếng bởi vô vàn những món ăn...</p>\n" +
+        "                                <p class=\"ppp\">By <a href=\"#\" style=\"text-decoration: none\" class=\"text-main-a1\">Kiều Oanh</a></p>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>"
+}
+var hd = document.getElementById("col_md_8");
+hd.innerHTML = col_md_8();
