9c31f01b1cf6d3d3f51a1fedf7557739580790ef
huytqth2008010 <truongquanghuybx0914@gmail.com>
2020-10-30 08:40:54 +0700

    huy cap nhat

---
+++ js/footer.js
@@ -0,0 +1,137 @@
+function footer() {
+    return "<footer class=\"page-footer bg-grey-dark-key\">\n" +
+        "    <div class=\"container\">\n" +
+        "        <div class=\"footer-top\">\n" +
+        "            <div class=\"row\">\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-2\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Công\n" +
+        "                            thức</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"https://review.food.com.vn/cong-thuc/do-uong/\">Món ăn</a></li>\n" +
+        "                                    <li><a href=\"https://review.food.com.vn/cong-thuc/mon-an/\">Đồ uống</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-3\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Địa\n" +
+        "                            điểm</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"https://review.food.com.vn/dia-diem/nha-hang/\">Nhà hàng</a></li>\n" +
+        "                                    <li><a href=\"/dia-diem/cafe/\">Cafe</a></li>\n" +
+        "                                    <li><a href=\"/dia-diem/quan-xa/\">Quán xá</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-4\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Sức\n" +
+        "                            khỏe</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"/suc-khoe/benh/\">Bệnh</a></li>\n" +
+        "                                    <li><a href=\"/suc-khoe/thuoc/\">Thuốc</a></li>\n" +
+        "                                    <li><a href=\"/suc-khoe/dinh-duong/\">Dinh dưỡng</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-5\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Làm\n" +
+        "                            đẹp</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"/lam-dep/my-pham/\">Mỹ phẩm</a></li>\n" +
+        "                                    <li><a href=\"/lam-dep/tu-nhien/\">Tự nhiên</a></li>\n" +
+        "                                    <li><a href=\"/lam-dep/tham-my/\">Thầm mỹ</a></li>\n" +
+        "                                    <li><a href=\"/lam-dep/bi-quyet/\">Bí quyết</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-6\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Du\n" +
+        "                            lịch</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"/du-lich/kham-pha/\">Khám phá</a></li>\n" +
+        "                                    <li><a href=\"https://food.com.vn/du-lich/van-hoa/\">Văn hóa</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <div class=\"footer-widget\">\n" +
+        "                        <div id=\"text-7\" class=\"widget footer-widget widget_text\"><h2 class=\"footer-widget-title\">Kinh\n" +
+        "                            nghiệm</h2>\n" +
+        "                            <div class=\"textwidget\">\n" +
+        "                                <ul class=\"footer-nav\">\n" +
+        "                                    <li><a href=\"/kinh-nghiem-hay/goc-bep/\">Góc bếp</a></li>\n" +
+        "                                    <li><a href=\"/kinh-nghiem-hay/meo-vat/\">Mẹo vặt</a></li>\n" +
+        "                                    <li><a href=\"/kinh-nghiem-hay/do-gia-dung/\">Đồ gia dụng</a></li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"footer-mid\">\n" +
+        "            <div class=\"row align-items-center\">\n" +
+        "                <div class=\"col-md-9\">\n" +
+        "                    <div class=\"footer-logo-container\">\n" +
+        "                        <a href=\"index.html\">\n" +
+        "                            <img src=\"https://review.food.com.vn/wp-content/uploads/2020/09/logo.png\" alt=\"footer logo\"\n" +
+        "                                 class=\"footer-logo\">\n" +
+        "                        </a>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-3\">\n" +
+        "                    <div class=\"footer-social-share-wrapper\">\n" +
+        "                        <div class=\"footer-social-share\">\n" +
+        "                            <div class=\"axil-social-title\">Find us here</div>\n" +
+        "                            <ul class=\"social-share social-share__with-bg\">\n" +
+        "                                <li><a class=\"icon1\" href=\"https://www.facebook.com/tapchifood/\"><i class=\"fab fa-facebook-f\"></i></a></li>\n" +
+        "                                <li><a class=\"icon2\" href=\"#\"><i class=\"fab fa-twitter\"></i></a></li>\n" +
+        "                                <li><a class=\"icon3\" href=\"#\"><i class=\"fab fa-instagram\"></i></a></li>\n" +
+        "                                <li><a class=\"icon4\" href=\"#\"><i class=\"fab fa-linkedin-in\"></i></a></li>\n" +
+        "                            </ul>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"footer-bottom\">\n" +
+        "            <ul class=\"footer-bottom-links\">\n" +
+        "                <li><a href=\"https://food.com.vn/\">Food Market</a></li>\n" +
+        "                <li><a href=\"#\">Giới thiệu</a></li>\n" +
+        "                <li><a href=\"#\">Chính sách bảo mật</a></li>\n" +
+        "                <li><a href=\"#\">Quy chế hoạt động</a></li>\n" +
+        "                <li><a href=\"#\">Thỏa thuận chia sẻ nội dung</a></li>\n" +
+        "                <li><a href=\"#\">Quảng cáo</a></li>\n" +
+        "                <li><a href=\"#\">Ban biên tập</a></li>\n" +
+        "                <li><a href=\"#\">Gửi bài</a></li>\n" +
+        "                <li><a href=\"#\">Thông tin liên hệ</a></li>\n" +
+        "                <li><a href=\"http://food.com.vn/sitemap_index.xml\">Sitemap</a></li>\n" +
+        "                <li><a href=\"http://food.com.vn/feed/\">Rss</a></li>\n" +
+        "            </ul>\n" +
+        "            <p class=\"axil-copyright-txt\">\n" +
+        "                © 2006 Food Review Magazine is a brand of Food Market Group (Singapore). All Rights Reserved.</p></div>\n" +
+        "    </div>\n" +
+        "</footer>"
+}
+var hd = document.getElementById("footer");
+hd.innerHTML = footer();
